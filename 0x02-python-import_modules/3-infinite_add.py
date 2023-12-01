1482917c733cae3d51b930c8254b79eae8c9c047
Titus Denis <deetitos@gmail.com>
2023-12-01 04:55:31 +0100

    3-infinite_add.py added

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    result = 0
+    if len(args) > 1:
+        for i in range(1, len(args)):
+            result += int(args[i])
+        print(result)
+    else:
+        print("{}".format(result));
+
