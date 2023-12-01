f201e5f63dac847cff4a514904d00c274eef8892
Titus Denis <deetitos@gmail.com>
2023-12-01 05:58:44 +0100

    100-my_calculator.py Added

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 100-my_calculator.py
+
+if __name__ == "__main__":
+    import calculator_1
+    import sys
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif len(args) == 4 and args[2] not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(ars[1])
+        b = int(ars[3])
+        operator = args[2]
+        if operator == "+":
+            print("{} {} {} = {}".format(a, operator, b, a + b))
+        elif operator == "-":
+            print("{} {} {} = {}".format(a, operator, b, a - b))
+        elif operator == "*":
+            print("{} {} {} = {}".format(a, operator, b, a * b))
+        else:
+            print("{} {} {} = {}".format(a, operator, b, a / b))
