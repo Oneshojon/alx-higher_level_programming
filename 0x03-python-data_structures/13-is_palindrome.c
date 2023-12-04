c3849be10f4421c8ee147eb05887392467058cbd
Titus Denis <deetitos@gmail.com>
2023-12-04 01:21:48 +0100

    13-is_palindrome.c, lists.h Added

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *is_palindrome - checks if a singly linked list is pallindrome
+ *@head: Pointer to the linked list
+ *
+ *Return: 1 if pallindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp;
+	int i = 0, j, k, *arr;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	temp = *head;
+	while(temp != NULL)
+	{
+		i++;
+		temp = temp->next;
+	}
+	arr = malloc(sizeof(int) * i);
+	if (arr == NULL)
+		return (0);
+	temp = *head;
+	for (j = 0; j < i; j++)
+	{
+		arr[j] = temp->n;
+		temp = temp->next;
+	}
+
+	j = 0;
+	for (j = 0, k = i - 1; j < k / 2; j++, k--)
+	{
+		if (arr[j] != arr[k])
+		{
+		/*	printf("%d = %d\n", (temp + j)->n, (temp + k)->n);*/
+			free(arr);
+			return (0);
+		}
+		/*printf("%d = %d\n", (temp + j)->n, (temp + k)->n);*/
+	}
+	free(arr);
+	return (1);
+}
