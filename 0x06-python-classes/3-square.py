07962b3fa6a381eec2ecbe5e385900be5dfcbb25
Titus Denis <deetitos@gmail.com>
2023-12-20 05:55:43 +0100

    3-square.py Added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class to represent a square
+
+
+    Attributes:
+    __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+
+        Args:
+        size (int): The size of the square. Default to zero.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Calculates and returns the area of a square.
+
+
+        Returns:
+        int: The area of the square.
+        """
+        return self.__size ** 2
