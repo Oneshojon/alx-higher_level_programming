566fe2d82b02af9573847a2fb3c358f58e0dcc8c
Titus Denis <deetitos@gmail.com>
2024-01-01 01:03:48 +0100

    100-singly_linked_list2.py

--- 0x06-python-classes/100-singly_linked_list2.py
+++ 0x06-python-classes/100-singly_linked_list2.py
@@ -45,4 +45,64 @@ def Node:
 	if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
+
+    @property
+    def next_node(self):
+        """
+        a getter methode to retrieve the pointer to the next Node.
+
+        returns:
+	pointer to the next Node.
+        """
+	return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """
+        setter: for assigning value to next_node.
+
+        Args:
+        value (Node object): A node object or None
+
+        Raises:
+        TypeError: If value is neither None nor Node.
+        """
+        if not isinstance(value, Node) and value != None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    """
+    Defines a singly linked list.
+
+    Artributes:
+    head: points to the linked list.
+    """
+    def __init__(self):
+        """
+        Initializes a new SinglyLinkedList.
+
+        Artributs:
+        head: points to an empty linked list.
+	"""
+        self.__head = None
+    def sorted_insert(self, value):
+        """
+        Sorts the Singly Linked list in acsending order.
+
+        Args:
+        value (int): value to be inserted into the sorted list.
+        """
+        new_node = Node(value)
+        if self.__head = None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data <\
+                    value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
     
