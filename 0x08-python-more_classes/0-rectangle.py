978a365eaa2a312872dbaadd2be93519b05a8315
Titus Denis <deetitos@gmail.com>
2024-01-04 23:06:57 +0100

    0-rectangle.py Added

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class to define an empty Rectangle class"""
+
+
+class Rectangle:
+    """
+    A class to define an empty rectangle
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
+    pass
