81002f68b560998cab20e4d32d344a82c0e3ab20
Titus Denis <deetitos@gmail.com>
2024-01-05 03:57:46 +0100

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -157,7 +157,8 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """
-        class method that returns a new Rectangle instance of equal width and height.
+        class method that returns a new Rectangle instance of equal
+        width and height.
 
         Args:
         size (int): size of the rectangle.
