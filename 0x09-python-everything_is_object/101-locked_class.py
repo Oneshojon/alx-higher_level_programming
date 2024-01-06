4667a976c6a5a8433b9b711f337f2cdcf65950d7
Titus Denis <deetitos@gmail.com>
2024-01-06 11:03:07 +0100

    101-locked_class.py Added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,7 +3,19 @@
 
 
 class LockedClass:
+    """
+    Locked classs that prevent instanciation
+
+    Args:
+        None
+    """
     __slots__ = ('first_name',)
 
     def __init__(self):
+        """
+        initializess nothing
+
+        Returns:
+        Nothing
+        """
         pass
