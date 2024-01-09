a0c6f062fe5913c7c1b16308a908e06ff13aba56
Titus Denis <deetitos@gmail.com>
2024-01-09 23:29:48 +0100

    0-read_file.py ADded

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Function to read a file
+"""
+
+
+def read_file(filename=""):
+    """
+    A function that reads a text file and prints it to stdout
+
+    Args:
+        filename: The name of file to read.
+
+    Returns: 
+        Nothing
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
