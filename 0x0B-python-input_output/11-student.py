21c69efc961ef40e2148d33b061262d9a646969f
Titus Denis <deetitos@gmail.com>
2024-01-10 18:41:21 +0100

    11-student.py Added

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Class that defines students."""
+
+
+class Student:
+    """
+    Creates a Student class.
+
+    Attributes:
+        to_json: Retireves dictionary representation of Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student class.
+
+        Args:
+            first_name (str): First name
+            last_name (str): Last name
+            age (int): Student Age
+
+        Return: A New instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method to retrieve dictionary representation of student instance.
+
+        Args:
+            Attr
+
+        Returns: Nothing
+        """
+        json_dict = {}
+        if isinstance(attrs, list):
+            if all(isinstance(item, str) for item in attrs):
+                for key, value in self.__dict__.items():
+                    if key in attrs:
+                        json_dict[key] = value
+        else:
+            return self.__dict__
+        return json_dict
+
+    def reload_from_json(self, json):
+        """
+        Method that replace all attributes of the Student instance.
+
+        Args:
+            json (dict): A dictionary of attribute names and values.
+
+        Returns:
+            Object with changed attribute values
+        """
+        for key, value in self.__dict__.items():
+            if key in json.keys():
+                setattr(self, key, json[key])
