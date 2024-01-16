b85f3dbffaca85c488cec33f64248472c8f91f60
Titus Denis <deetitos@gmail.com>
2024-01-16 14:35:23 +0100

    models/base.py Added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -92,6 +92,9 @@ class Base:
         """
         Loads a list of instances from a file.
 
+        Args:
+            cls
+
         Returns:
            list: List of instances loaded from the file.
         """
@@ -101,5 +104,5 @@ class Base:
                 json_string = f.read()
                 list_of_dicts = cls.from_json_string(json_string)
                 return [cls.create(**d) for d in list_of_dicts]
-            except FileNotFoundError:
-                return []
+        except FileNotFoundError:
+            return []
