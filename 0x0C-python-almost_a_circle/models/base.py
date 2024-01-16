66bd2874e3068aa6caeae0a27805ffc96f7a6a19
Titus Denis <deetitos@gmail.com>
2024-01-16 13:36:49 +0100

    models/base.py Added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -71,7 +71,7 @@ class Base:
         """
         if json_string is None or len(json_string) == 0:
             return []
-        return json_string
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
