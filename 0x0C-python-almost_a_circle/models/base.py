25dadcd2bc2a548ebeb16bf91a6fe07199f1050f
Titus Denis <deetitos@gmail.com>
2024-01-16 11:34:18 +0100

    models/base.py Added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -72,3 +72,34 @@ class Base:
         if json_string is None or json_string == []:
             return []
         return json_string
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Loads instance from dictionary.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''Saves object to csv file.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[o.id, o.width. o.height, o.x, o.y]
+                             for o in list_objs]
+            else:
+                list_objs = [[o.id, o.size, o.x, o.y]
+                             for o in list_objs]
+        with open("{}.csv".format(cls.__name__), 'w', newline='',
+                  encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerows(list_objs)
